b08fd4d728270696c7478a70601593653aa0c996
mramos.dev423 <mramosg@fcpn.edu.bo>
2023-07-03 22:01:27 -0400

    Numeros, cadenas

---
+++ Basico/NumerosObjetoMath.js
@@ -0,0 +1,35 @@
+// Numeros
+let edad=45 //
+let gravedad=9.81 //
+// const PI=3.14 //
+let negativo=-5
+console.log(edad , typeof edad);
+console.log(gravedad , typeof gravedad);
+// console.log(PI , typeof PI);
+// console.log(negativo , typeof PI);
+//! Objeto Matematico (Math)
+const PI=Math.PI
+console.log(PI); //3.141592653589793
+console.log(Math.round(PI));//3
+console.log(Math.round(9.81)); //10
+console.log(Math.floor(PI));//3
+console.log(Math.floor(9.81)); //9
+console.log(Math.ceil(PI)); //4
+console.log(Math.ceil(9.81)); //10
+console.log(Math.min(-5,3,4,5,0))
+console.log(Math.max(-5,3,4,5,0))
+const random= Math.random() //(0 - 0.9999999) 
+console.log(random);
+
+const numeroGenerado10=Math.floor(Math.random()*11) //(0-10) 11
+console.log(numeroGenerado10);
+
+console.log(Math.abs(-10)); //10
+console.log(Math.sqrt(100)); //10
+console.log(Math.pow(3,2)); //9
+console.log(Math.sin(0));
+console.log(Math.sin(60));
+console.log(Math.cos(0));
+console.log(Math.cos(60));
+
+
--- Basico/Operadores.js
+++ Basico/Operadores.js
@@ -60,7 +60,57 @@ console.log('3==="3"',3==='3');
 console.log('3!=="3"',3!=='3');
 console.log('0==false',0==false);
 console.log('0===false',0===false);
+console.log('word'.length == 'palabras'.length );
 
+//! Operadores Lógicos
+//* && (and)
+//* || (or)
+//* ! (not)
+const check= 4>3 && 10>5
+const check1= 4>3 && 10<5
+console.log(check);
+console.log(check1);
+const check2= 4>3 || 10>5
+const check3= 2>3 || 10<5
+console.log(check2);
+console.log(check3);
+let check4=!(4<3)
+console.log(check4);
+
+//! OPERADOR DE INCREMENTO
+// Pre incremento
+let contador1=0
+console.log(++contador1);
+console.log(++contador1);
+console.log(++contador1);
+// Pre decremento
+console.log(--contador1);
+console.log(--contador1);
+
+
+// Post incremento
+let contador=0
+// contador=contador+1 //1
+// contador=contador+1 //2
+// contador=contador+1 //3
+// contador=contador+1 //4
+
+console.log(contador++);
+console.log(contador++);
+console.log(contador++);
+console.log(contador);
+// Post decremento
+console.log(contador--);
+console.log(contador);
+
+// if(2>6){
+//     console.log('El numero 2 es mayor a cero');
+// }else{
+//     console.log('El numero 2 no es mayor a cero');
+// }
+//Operador ternario
+let numero=-5
+numero > 0 ? console.log('El numero es positivo'):console.log('El numero no es positivo');
 
 
 
